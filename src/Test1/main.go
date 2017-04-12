eaa935797d5a27ce12a863689b796b382a4abcf1
foreverm99 <foreverm99@163.com>
2017-04-13 00:53:37 +0800

    add some new functions

--- src/Test1/main.go
+++ src/Test1/main.go
@@ -7,7 +7,24 @@ import (
 	"./pack1"
 )
 
+type struct1 struct {
+	i1  int
+	f1  float32
+	str string
+}
+
 func main() {
+	ms := new(struct1)
+	ms.i1 = 10
+	ms.f1 = 15.5
+	ms.str = "Chris"
+	fmt.Printf("The int is: %d\n", ms.i1)
+	fmt.Printf("The float is :%f\n", ms.f1)
+	fmt.Printf("The string is :%s\n", ms.str)
+	fmt.Println(ms)
+}
+
+func main22() {
 	var test1 string
 	test1 = pack1.ReturnStr()
 	fmt.Printf("ReturnStr from package1: %s\n", test1)
